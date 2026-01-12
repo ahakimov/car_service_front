c89cdf6f2765c17dc097260af27ef973f7649f14
Abdulkhakim Khakimov <a.khakimov.22@gmail.com>
2026-01-12 12:38:47 +0100

    Add login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from "@/modules/login/ui/LoginPage";
+
+export default function Home() {
+  return (
+      <main className="min-h-screen">
+        <LoginPage />
+      </main>
+  );
+}
