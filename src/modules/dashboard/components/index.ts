8e9e646eb15d596a0268f6204e9a34f75c895934
Abdulkhakim Khakimov <a.khakimov.22@gmail.com>
2026-01-24 14:53:37 +0100

    Remove unused code and finalize

---
+++ src/modules/dashboard/components/index.ts
@@ -0,0 +1,51 @@
+// Dashboard Components - Core
+export { Sidebar } from './Sidebar';
+export { ManagerSidebar } from './ManagerSidebar';
+export { ClientSidebar } from './ClientSidebar';
+export { MechanicSidebar } from './MechanicSidebar';
+export { MetricCard } from './MetricCard';
+export { Notification } from './Notification';
+export { ScheduleHeader } from './ScheduleHeader';
+export { ScheduleCalendar } from './ScheduleCalendar';
+export { CalendarEventComponent } from './CalendarEventComponent';
+export { DeleteModal } from './DeleteModal';
+
+// Tables
+export { ReservationTable } from './ReservationTable';
+export { ClientReservationsTable } from './ClientReservationsTable';
+export { ClientsTable } from './ClientsTable';
+export { MechanicsTable } from './MechanicsTable';
+export { VisitorRequestsTable } from './VisitorRequestsTable';
+
+// Detail Modals
+export { ReservationDetailsModal } from './ReservationDetailsModal';
+export { ClientDetailsModal } from './ClientDetailsModal';
+export { MechanicDetailsModal } from './MechanicDetailsModal';
+export { RepairJobDetailsModal } from './RepairJobDetailsModal';
+export { ClientRepairJobDetailsModal } from './ClientRepairJobDetailsModal';
+export { VehicleHistoryModal } from './VehicleHistoryModal';
+export { VisitorRequestDetailsModal } from './VisitorRequestDetailsModal';
+
+// Create/Add Modals
+export { CreateReservationModal } from './CreateReservationModal';
+export { ClientCreateReservationModal } from './ClientCreateReservationModal';
+export { CreateClientModal } from './CreateClientModal';
+export { CreateMechanicModal } from './CreateMechanicModal';
+export { CreateRepairJobModal } from './CreateRepairJobModal';
+export { AddEventModal } from './AddEventModal';
+export { AddVehicleModal } from './AddVehicleModal';
+
+// Filter Modals
+export { ReservationFilterModal } from './ReservationFilterModal';
+export { ClientReservationFilterModal } from './ClientReservationFilterModal';
+export { ClientFilterModal } from './ClientFilterModal';
+export { MechanicFilterModal } from './MechanicFilterModal';
+export { ScheduleFilterModal } from './ScheduleFilterModal';
+export { ClientRepairJobFilterModal } from './ClientRepairJobFilterModal';
+export { MechanicReservationFilterModal } from './MechanicReservationFilterModal';
+export { MechanicRepairJobFilterModal } from './MechanicRepairJobFilterModal';
+
+// Settings Components
+export { ProfileTab } from './ProfileTab';
+export { PasswordTab } from './PasswordTab';
+export { ProfileAvatar } from './ProfileAvatar';
